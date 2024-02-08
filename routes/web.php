fed6c5bf8e514fe014246586d3f8afefc3243c2c
Travis Sheldon <travis@ownwebnow.com>
2024-02-08 09:50:50 -0500

    add blade icons caching

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Middleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/_', 'App\Http\Controllers\FirstTimeRunController');
+Route::get('/_status', fn() => response("OK"))
+    ->withoutMiddleware([
+        Middleware\ApplyPersistedSettingsMiddleware::class,
+        Middleware\ChangeLayoutMiddleware::class,
+        Middleware\CheckSiteConfiguredMiddleware::class,
+        Middleware\CheckArchiveHealthMiddleware::class,
+    ])
+->name('status');
 
 Route::redirect('/', '/mailbox')->name('home');
 
